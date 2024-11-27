5667cb209da9faa7e29ac2fd4805f0b8863bdf6c
Yehor Lesnevych <egor.lesnev@ukr.net>
2024-11-27 22:10:52 +0200

    Add put and delete for files

--- PatternsProject/HRMS/Controllers/FileController.cs
+++ PatternsProject/HRMS/Controllers/FileController.cs
@@ -2,16 +2,18 @@
 using ApplicationCore.CQRS.Queries.File;
 using ApplicationCore.Models;
 using ApplicationCore.Models.Dto.File;
+using ApplicationCore.Models.Dto.Request;
 using ApplicationCore.Services.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
 
 namespace HRMS.Controllers;
 
 [Route("files")]
 [RequestSizeLimit(2_000_000_000)]
 [RequestFormLimits(MultipartBodyLengthLimit = 2_000_000_000)]
-public class FileController(IMediator mediator, IMapper mapper) : BaseController(mediator)
+public class FileController(IMediator mediator, IMapper mapper, IFileService fileService) : BaseController(mediator)
 {
 	[HttpGet("{requestId}")]
 	public async Task<IActionResult> GetFilesByUser(Guid requestId)
@@ -27,6 +29,38 @@ public class FileController(IMediator mediator, IMapper mapper) : BaseController
 		var command = mapper.Map<CreateFileCommand>(dto);
 
 		await Mediator.SendAsync(command);
+
+		return Ok();
+	}
+
+	[HttpPut]
+	public async Task<IActionResult> UpdateFile(Guid fileId, [FromBody] UpdateFileDto dto)
+	{
+		var file = await fileService.GetAsync(x => x.FileId == fileId);
+
+		if(file is null)
+			return NotFound();
+
+		using var memoryStream = new MemoryStream();
+
+		dto.FileContent.CopyTo(memoryStream);
+
+		file.FileContent = memoryStream.ToArray();
+
+		await fileService.UpdateAsync(file);
+
+		return Ok();
+	}
+
+	[HttpDelete]
+	public async Task<IActionResult> UDeleteFile([FromQuery] Guid fileId)
+	{
+		var file = await fileService.GetAsync(x => x.FileId == fileId);
+
+		if (file is null)
+			return NotFound();
+
+		await fileService.DeleteAsync(file);
 
 		return Ok();
 	}
